acd4412c20866e6246c3151f1d40a27dbd06cbe0
Luis Jimenez <luisejr20@gmail.com>
2023-01-27 16:51:25 -0600

    Update 2 - Create Book Table + Data.sql

--- DB-Scripts/2 - Create Book Table + Data.sql
+++ DB-Scripts/2 - Create Book Table + Data.sql
@@ -23,20 +23,57 @@ PRIMARY KEY CLUSTERED
 GO
 SET IDENTITY_INSERT [dbo].[books] ON 
 GO
-INSERT [dbo].[books] ([book_id], [title], [first_name], [last_name], [total_copies], [copies_in_use], [type], [isbn], [category]) VALUES (1, N'Pride and Prejudice', N'Jane', N'Austen', 100, 80, N'Hardcover', N'1234567891', N'Fiction')
-GO
-INSERT [dbo].[books] ([book_id], [title], [first_name], [last_name], [total_copies], [copies_in_use], [type], [isbn], [category]) VALUES (2, N'To Kill a Mockingbird', N'Harper', N'Lee', 75, 65, N'Paperback', N'1234567892', N'Fiction')
-GO
-INSERT [dbo].[books] ([book_id], [title], [first_name], [last_name], [total_copies], [copies_in_use], [type], [isbn], [category]) VALUES (3, N'The Catcher in the Rye', N'J.D.', N'Salinger', 50, 45, N'Hardcover', N'1234567893', N'Fiction')
-GO
-INSERT [dbo].[books] ([book_id], [title], [first_name], [last_name], [total_copies], [copies_in_use], [type], [isbn], [category]) VALUES (4, N'The Great Gatsby', N'F. Scott', N'Fitzgerald', 50, 22, N'Hardcover', N'1234567894', N'Non-Fiction')
-GO
-INSERT [dbo].[books] ([book_id], [title], [first_name], [last_name], [total_copies], [copies_in_use], [type], [isbn], [category]) VALUES (5, N'The Alchemist', N'Paulo', N'Coelho', 50, 35, N'Hardcover', N'1234567895', N'Biography')
-GO
-INSERT [dbo].[books] ([book_id], [title], [first_name], [last_name], [total_copies], [copies_in_use], [type], [isbn], [category]) VALUES (6, N'The Book Thief', N'Markus', N'Zusak', 75, 11, N'Hardcover', N'1234567896', N'Mystery')
-GO
-INSERT [dbo].[books] ([book_id], [title], [first_name], [last_name], [total_copies], [copies_in_use], [type], [isbn], [category]) VALUES (7, N'Bride and Justice', N'Jimmy', N'Mcgill', 100, 80, N'Hardcover', N'1234567898', N'Lawyers')
-GO
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('Pride and Prejudice', 'Jane', 'Austen', 100, 80, 'Hardcover', '1234567891', 'Fiction');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('To Kill a Mockingbird', 'Harper', 'Lee', 75, 65, 'Paperback', '1234567892', 'Fiction');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Catcher in the Rye', 'J.D.', 'Salinger', 50, 45, 'Hardcover', '1234567893', 'Fiction');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Great Gatsby', 'F. Scott', 'Fitzgerald', 50, 22, 'Hardcover', '1234567894', 'Non-Fiction');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Alchemist', 'Paulo', 'Coelho', 50, 35, 'Hardcover', '1234567895', 'Biography');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Book Thief', 'Markus', 'Zusak', 75, 11, 'Hardcover', '1234567896', 'Mystery');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Chronicles of Narnia', 'C.S.', 'Lewis', 100, 14, 'Paperback', '1234567897', 'Sci-Fi');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Da Vinci Code', 'Dan', 'Brown', 50, 40, 'Paperback', '1234567898', 'Sci-Fi');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Grapes of Wrath', 'John', 'Steinbeck', 50, 35, 'Hardcover', '1234567899', 'Fiction');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Hitchhiker''s Guide to the Galaxy', 'Douglas', 'Adams', 50, 35, 'Paperback', '1234567900', 'Non-Fiction');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('Moby-Dick', 'Herman', 'Melville', 30, 8, 'Hardcover', '8901234567', 'Fiction');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('To Kill a Mockingbird', 'Harper', 'Lee', 20, 0, 'Paperback', '9012345678', 'Non-Fiction');
+
+
+INSERT INTO books (title, first_name, last_name, total_copies, copies_in_use, type, isbn, category)
+VALUES ('The Catcher in the Rye', 'J.D.', 'Salinger', 10, 1, 'Hardcover', '0123456789', 'Non-Fiction');
+
 SET IDENTITY_INSERT [dbo].[books] OFF
 GO
 ALTER TABLE [dbo].[books] ADD  DEFAULT ((0)) FOR [total_copies]
